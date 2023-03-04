3a44db04455541a0e58f67b2beb4206b4c88a19d
Stelios Miskedakis <123480703+steliosmisk@users.noreply.github.com>
2023-03-04 08:53:27 +0200

    Update animalage.py

--- animalage.py
+++ animalage.py
@@ -1,25 +1,28 @@
-print("--WELCOME TO ANIMAL AGE CALCULATOR--")
+print("-- WELCOME TO ANIMAL AGE CALCULATOR --")
 print("[1] Cat to Human Age")
 print("[2] Dog to Human Age")
 print("[3] Exit")
 
 user_choice = int(input("Enter (1/2/3): "))
-while user_choice in {1, 2} and user_choice != 3:
+
+while user_choice in {1, 2}:
     try:
-        total_age = 0
         if user_choice == 1:
             cat_age = int(input("Enter the cat's age: "))
             if cat_age == 1:
-                total_age = 15
+                human_age = 15
             elif cat_age == 2:
-                total_age = 25
+                human_age = 25
             else:
-                total_age = 25 + (cat_age - 2) * 4
+                human_age = 25 + (cat_age - 2) * 4
+            print("Your cat's age is equivalent to", human_age, "human years!")
         else:
             dog_age = int(input("Enter the dog's age: "))
-            total_age = dog_age * 11 if dog_age in {1, 2} else 22 + (dog_age - 2) * 4
-        print("Your pet's age is:", total_age, "human years!")
+            if dog_age in {1, 2}:
+                human_age = dog_age * 11
+            else:
+                human_age = 22 + (dog_age - 2) * 4
+            print("Your dog's age is equivalent to", human_age, "human years!")
         break
     except ValueError:
-        print("Please enter a valid age.") 
-
+        print("Please enter a valid age.")
